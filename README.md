9e421f2ac6d6e75e6c20714da3af0f9ca6041695
Ashokreddy0416 <ashoksku416@gmail.com>
2026-01-08 00:24:30 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # END_TO_END_RAG
+```bash
+git clonehttps://github.com/Ashokreddy0416/END_TO_END_RAG.git
+```
+```bash
+conda create -n medibot1 python=3.10 -y
+```
+```bash
+conda activate medibot1
+```
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="gadibavi ashok kumar reddy",
+    author_email="ashoksku416@gmail.com",
+    packages=find_packages(),
+    install_requires=[])
